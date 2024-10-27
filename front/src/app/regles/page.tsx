081ce7c58043d2655c216b7cd36762113b2f6155
amine RABBOUCH <amine.rabbouch@gmail.com>
2024-10-27 18:31:16 +0100

    correction erreur deplyment

--- front/src/app/regles/page.tsx
+++ front/src/app/regles/page.tsx
@@ -28,12 +28,12 @@ export default function Regles() {
                                 <AccordionContent className="text-gray-700 mt-2 leading-relaxed">
                                     <p className="mb-4">
                                         <strong>La phase de pool :</strong> Les 28 joueurs sont répartis en fonction du seeding en 8 groupes de 5 joueurs.
-                                        À l'issue de la phase de poules, les 2 premiers de chaque groupe iront en winner bracket, les 2 derniers en loser bracket.
+                                        À l&apos;issue de la phase de poules, les 2 premiers de chaque groupe iront en winner bracket, les 2 derniers en loser bracket.
                                         Les matchs se déroulent en Best of 3 en fonction des règles de bannissements de terrains ci-dessous.
                                     </p>
                                     <p>
                                         <strong>La phase de brackets :</strong> Les joueurs sont répartis en fonction de leurs résultats de pools.
-                                        Les matchs se déroulent en Best of 5. La moitié dans l'arbre winner, l'autre dans l'arbre loser. 
+                                        Les matchs se déroulent en Best of 5. La moitié dans l&apos;arbre winner, l&apos;autre dans l&apos;arbre loser. 
                                         Ceux qui perdent un match en winner bracket sont envoyés dans la loser brackets, ceux qui perdent en loser bracket sont définitivement éliminés.
                                         La finale du tournoi a lieu entre le dernier joueur de la winner bracket et le dernier joueur de la loser bracket.
                                     </p>
@@ -41,7 +41,7 @@ export default function Regles() {
                             </AccordionItem>
                             <AccordionItem value="item-3">
                                 <AccordionTrigger className="text-xl font-semibold text-blue-600">
-                                    Déroulement d'un match
+                                    Déroulement d&apos;un match
                                 </AccordionTrigger>
                                 <AccordionContent className="text-gray-700 mt-2 leading-relaxed">
                                     <Image
@@ -53,12 +53,12 @@ export default function Regles() {
                                         priority
                                     />
                                     <p className="mb-4 text-center">
-                                        Les règles lors d'un set de Smash Bros Ultimate dans le Rabbouch Project X sont indiquées ci-dessous.
-                                        En cas de timeout (le temps arrive à 0), c'est celui avec le plus de vies qui remporte le match. 
+                                        Les règles lors d&apos;un set de Smash Bros Ultimate dans le Rabbouch Project X sont indiquées ci-dessous.
+                                        En cas de timeout (le temps arrive à 0), c&apos;est celui avec le plus de vies qui remporte le match. 
                                         Si les deux joueurs ont le même nombre de vies, celui qui a le moins de pourcentage de dommages gagne.
                                     </p>
                                     <p className="text-center">
-                                        Concernant les personnages jouables, il n'y a pas de restrictions : tout le monde peut jouer tous les personnages.
+                                        Concernant les personnages jouables, il n&apos;y a pas de restrictions : tout le monde peut jouer tous les personnages.
                                         Toutefois, avant de bannir les stages, les deux joueurs doivent annoncer leurs personnages.
                                     </p>
                                 </AccordionContent>
